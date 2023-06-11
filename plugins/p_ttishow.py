7c8e7abb194287972f5adc70e5f234a47d7bf80b
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-11 09:15:58 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -56,8 +56,8 @@ async def save_group(bot, message):
                 temp.MELCOW['welcome'] = await message.reply_video(
                 video="https://telegra.ph/file/03691465baa774e46506d.mp4",                                               
                                                  caption=f'<b>ʜᴇʏ, {u.mention} 👋🏻\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ɢʀᴏᴜᴘ {message.chat.title}\n\nFind Any Media ! if you need any movie then then enter the movie name + years. 👍\n\nGuys Enter Only movie Or Webseries Name like This 👇\nVarisu ✅\nVarisu 2023 ✅\nVarisu in Tamil ❌\nFall ✅\nFall S01 ✅\nFall all season ❌</b>',
-                                                 reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥  ↭ Main Channel ↭  🔥', url='http://t.me/TN68_FilmFactory') ],
-                                                                                      [ InlineKeyboardButton('🔥  ↭ DEV Channel ↭  🔥', url='https://t.me/+rN9QCFgIihgyZWM1') ]
+                                                 reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥  ↭ Main Channel ↭  🔥', url='https://t.me/ThammuTV') ],
+                                                                                      [ InlineKeyboardButton('🔥  ↭ DEV Channel ↭  🔥', url='https://t.me/ThammuTV') ]
                                                                                     ] )
                 )
 
